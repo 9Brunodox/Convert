9dbadf4cb5a4b8cead6ecb88151377b5f1690797
9Brunodox <bruno.moreiradasilva.2002@gmail.com>
2025-01-29 21:13:24 -0300

    Encerrando o projeto, feito a conversão, exibindo o resultado e formatando ele para o padrão BRL

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const GBP = 7.32;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -32,5 +35,34 @@ form.onsubmit = (e) => {
 };
 
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol);
+  try {
+    // Exibindo a cotação da moeda selecionada
+    description.textContent = `${symbol}1 = ${formatCurrencyBRL(price)}`;
+
+    // Calcula o total da conversão
+    let total = amount * price
+
+    // Exibe o resultado da conversão
+    result.textContent = `${formatCurrencyBRL(total)}`
+
+
+    // Aplica a classe que exibe o footer.
+    footer.classList.add("show-result");
+  } catch (error) {
+    console.log(error);
+
+    // Remove a classe que exibe o footer e exibe uma mensagem de erro.
+    footer.classList.remove("show-error");
+    alert("Ocorreu um erro durante a conversão.");
+  }
+}
+
+
+// Formata a moeda em real brasileiro
+function formatCurrencyBRL(value){
+  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  });
 }
